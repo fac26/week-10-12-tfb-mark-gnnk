98033a042352ca51f8e9a8f2f5716e2792c52785
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-03-24 14:57:57 +0000

    initial date component layout

--- src/components/layout/Date.js
+++ src/components/layout/Date.js
@@ -1,1 +1,6 @@
 import { parseISO, format, addDays } from 'date-fns'
+
+export default function Date({dateString}) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "EEEE do LLLL y")}</time>;
+}
