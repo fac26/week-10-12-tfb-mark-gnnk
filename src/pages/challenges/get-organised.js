1da4c164a5a4a8e32586716e31196ea1cdc910be
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-03-26 09:47:39 +0100

    get data from db to render list of get-organised tasks

--- src/pages/challenges/get-organised.js
+++ src/pages/challenges/get-organised.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
-export default function GetOrganized() {
+export default function GetOrganized({ tasks }) {
+	console.log(tasks)
 	return (
 		<>
 			return <h1>Get Organized!</h1>
@@ -10,6 +11,35 @@ export default function GetOrganized() {
 				height={600}
 				alt="get organized challenge screenshot"
 			/>
+			<ul>
+				{tasks.map((task) => (
+					<li
+						key={task.id}
+						id={task.id}>
+						<h2>{task.name}</h2>
+						<p>{task.category_name}</p>
+					</li>
+				))}
+			</ul>
 		</>
 	)
+}
+
+export async function getServerSideProps(context) {
+	//const userId = context.req.session.userId
+
+	const userId = 1
+	// Fetch the tasks data for the user from the API endpoint
+	const response = await fetch(
+		`http://localhost:3000/api/get-organised?userId=${userId}`
+	)
+	const tasks = await response.json()
+	console.log(tasks, ' getOragnised.js')
+
+	// Pass the tasks data as a prop to the page component
+	return {
+		props: {
+			tasks
+		}
+	}
 }
