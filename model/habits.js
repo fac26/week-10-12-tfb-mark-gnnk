506f100ba75d42464886c2b0f77a2747e24a930b
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-03-26 09:31:31 +0100

    fix select due to updated schema

--- model/habits.js
+++ model/habits.js
@@ -1,8 +1,8 @@
 import db from '../database/db.js'
 
 const select_all_habits = db.prepare(/*sql*/ `
-    SELECT habit_catagories.id, habit_catagories.name
-    FROM habit_catagories
+    SELECT habit_categories.id, habit_categories.name
+    FROM habit_categories
     `)
 
 export function getAllHabits() {
