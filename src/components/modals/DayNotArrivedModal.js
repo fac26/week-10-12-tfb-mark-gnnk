ea2ed28011b807a59e9bf9228e0676f99a7c32ac
Konstantina Katmada <konstantinakatmada@gmail.com>
2023-03-27 11:21:55 +0100

    import of styles removed

--- src/components/modals/DayNotArrivedModal.js
+++ src/components/modals/DayNotArrivedModal.js
@@ -1,17 +1,17 @@
 import React from 'react';
-//import styles from '/styles/DayNotArrivedModal.css';
+
 
 
 function DayNotArrivedModal(props) {
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <h2>Jumping ahead?</h2>
-        <p>This day has not arrived yet. Please come back for this page tomorrow.</p>
-        <button onClick={props.onClose}>OK!</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                <h2>Jumping ahead?</h2>
+                <p>This day has not arrived yet. Please come back for this page tomorrow.</p>
+                <button onClick={props.onClose}>OK!</button>
+            </div>
+        </div>
+    );
 }
 
 export default DayNotArrivedModal;
