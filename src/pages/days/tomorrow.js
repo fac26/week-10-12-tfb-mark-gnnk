a157f2a9dc93a913313170b55ff2c3238e5f0489
Konstantina Katmada <konstantinakatmada@gmail.com>
2023-03-27 12:25:16 +0100

    commit to merge with m ain

--- src/pages/days/tomorrow.js
+++ src/pages/days/tomorrow.js
@@ -5,7 +5,7 @@ import DayNotArrivedModal from '../../components/modals/DayNotArrivedModal';
 export default function Tomorrow(props) {
 	const [showModal, setShowModal] = useState(false);
 
-//add this function after creating the DateCard component
+	//add this function after creating the Days and DateCard component
 	// const handleCardClick = () => {
 	// 	const today = new Date();
 	// 	const tomorrow = new Date(today);
@@ -20,12 +20,13 @@ export default function Tomorrow(props) {
 	// 	}
 	// };
 
+
 	//test Modal with button that calls the setShowModal function to set the showModal state to true
 	const handleOpenModal = () => {
 		setShowModal(true);
-	  };
+	};
 	//end of testing Modal
-	
+
 	const handleCloseModal = () => {
 		setShowModal(false);
 	};
@@ -35,20 +36,20 @@ export default function Tomorrow(props) {
 		//button that calls the setShowModal function
 		// to set the showModal state to true
 		<div>
-      <h1>Tomorrow</h1>
-      <button onClick={handleOpenModal}>Open Modal</button>
-      {showModal && (
-        <DayNotArrivedModal onClose={handleCloseModal} />
-      )}
-    </div>
-  );
-//code after creating the DateItem,DayList components
-		// <div>
-		// 	{/* Render DateCard for tomorrow's day here */}
-		// 	<button onClick={handleCardClick}>Click for tomorrow's day</button>
-		// 	{showModal && (
-		// 		<DayNotArrivedModal onClose={handleCloseModal} />
-		// 	)}
-		// </div>
+			<h1>Tomorrow</h1>
+			<button onClick={handleOpenModal}>Open Modal</button>
+			{showModal && (
+				<DayNotArrivedModal onClose={handleCloseModal} />
+			)}
+		</div>
+	);
+	//code after creating the DateItem,Day components
+	// <div>
+	// 	{/* Render DateCard for tomorrow's day here */}
+	// 	<button onClick={handleCardClick}>Click for tomorrow's day</button>
+	// 	{showModal && (
+	// 		<DayNotArrivedModal onClose={handleCloseModal} />
+	// 	)}
+	// </div>
 	//);
 }
