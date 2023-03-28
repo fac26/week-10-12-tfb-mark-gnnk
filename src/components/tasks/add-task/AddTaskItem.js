7cff7bac91110da5c4dbbb71fe82ef2d4d84a97f
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-03-28 16:03:25 +0100

    created list item with empty classNames, importing Button component and addTask

--- src/components/tasks/add-task/AddTaskItem.js
+++ src/components/tasks/add-task/AddTaskItem.js
@@ -1,5 +1,20 @@
 //task with button <Add> that listens for event when user clickes add button
-function AddTaskItem({ task }) {
-	return <></>
+import Button from 'components/UI/Button'
+import { addTask } from 'model/tasks'
+
+export default function AddTaskItem({ task }) {
+	return (
+		<li className="">
+			<div className="">
+				<h3>{task.name}</h3>
+				<p>{task.category_name}</p>
+				<Button
+					background="bg_blu"
+					scaling="medium"
+					onClick={addTask(task.id)}>
+					ADD
+				</Button>
+			</div>
+		</li>
+	)
 }
-export default AddTaskItem
