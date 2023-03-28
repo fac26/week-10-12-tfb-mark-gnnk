3cb0d2dc753c0e3aaf79156b8c34e3119477db24
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-03-28 16:25:34 +0100

    rectangle-changes

--- src/components/cards/Rectangle.js
+++ src/components/cards/Rectangle.js
@@ -1,13 +1,11 @@
-// component that is a wrapper so that it renders a div with border-radius that is not sharp
-import Days from '../layout/Days'
 import styles from './Rectangle.module.css'
 
-export default function Rectangle({ backgroundColor, children}) {
+export default function Rectangle({ backgroundColor, children }) {
 	return (
-			<div
-				className={styles.card}
-				style={{ backgroundColor }}>
-        {children}
-			</div>
+		<div
+			className={styles.card}
+			style={{ backgroundColor }}>
+			{children}
+		</div>
 	)
 }
