f4e758e5088878aa78c298c4fb5aaaab8be64d37
Niete <nieteis@gmail.com>
2023-03-29 10:14:17 +0100

    Todays Challenges page and css module created
    Co-authored-by: Natalia Rusu <nataliarusu@users.noreply.github.com>

--- src/pages/challenges/index.js
+++ src/pages/challenges/index.js
@@ -1,23 +1,54 @@
+import Link from 'next/link'
 import Image from 'next/image'
 import { getAllHabits } from '../../../model/habits'
+import Days from 'components/layout/Days'
+import ProgressCircle from 'components/cards/ProgressCircle'
+import styles from './Challenges.module.css'
 
 export default function Challenges({ tasks, habits }) {
 	console.log(habits)
 	return (
-		<>
-			<h1>Challenges</h1>
-			<Image
-				src="/figma/challenges.png"
-				width={300}
-				height={600}
-				alt="challenges screenshot"
-			/>
-			<ul>
+		<div className={styles.background}>
+			<div className={styles.container}>
+				<div className={styles.heading}>
+					<p>Your challenges for:</p>
+					<div className={styles.date}>
+						<Days type={'today'} />
+					</div>
+				</div>
+				<div>
+					<ProgressCircle percentage={50} />
+				</div>
+			</div>
+			<div className={styles.challengeHeader}>
+				<p>Pick a challenge for today</p>
+			</div>
+
+			<ul className={styles.habitContainer}>
 				{habits.map((habit) => (
-					<li key={habit.id}>{habit.name}</li>
+					<li key={habit.id}>
+						<Link
+							href={`/challenges/${habit.name.toLowerCase().replace(' ', '-')}`}
+							className={styles.challengeBox}>
+							<div>
+								<h3>{habit.name}</h3>
+								<p>Micro-habits to help you {habit.name}</p>
+							</div>
+							<div>
+								<ProgressCircle
+									percentage={50}
+									textColor="red"
+									pathColor="var(--main-lavendar)"
+									trailColor="transparent"
+									width={75}
+									strokeWidth={8}
+								/>
+							</div>
+						</Link>
+					</li>
 				))}
 			</ul>
-		</>
+		</div>
 	)
 }
 
