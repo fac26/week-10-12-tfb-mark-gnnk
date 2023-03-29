5ff55e5ca5a15ce14a3df075131dda8a8a9ae6e6
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-03-29 16:14:27 +0100

    fetch tasks logic added to page and new api route

---
+++ src/pages/api/get-prepared.js
@@ -0,0 +1,17 @@
+import { getAllCurrentTasksByCategory } from 'model/tasks'
+
+export default async function handler(req, res) {
+	try {
+		const userId = req.query.userId
+		console.log(userId, 'user id')
+		if (req.method === 'GET') {
+			const tasks = getAllCurrentTasksByCategory(userId, 2)
+			console.log(tasks, 'tasks from api')
+
+			res.status(200).json(tasks)
+		}
+	} catch (err) {
+		console.error(err)
+		res.status(500).json({ error: 'Internal server error' })
+	}
+}
