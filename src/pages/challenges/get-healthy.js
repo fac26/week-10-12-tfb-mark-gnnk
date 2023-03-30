1c245138c579a115c421b05f8b2198305372e4de
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-03-30 11:23:47 +0100

    repopulated get-healthy

--- src/pages/challenges/get-healthy.js
+++ src/pages/challenges/get-healthy.js
@@ -1,15 +1,69 @@
 import Image from 'next/image'
+import HeaderCard from 'components/cards/HeaderCard'
+import ChallengeCategoryList from 'components/challenges/ChallengeCategoryList'
 
-export default function GetHealthy() {
-	return (
-		<>
-			<h1>Get Healthy!</h1>
-			<Image
-				src="/figma/get-healthy.png"
-				width={300}
-				height={600}
-				alt="get healthy challenge screenshot"
+export default function GetHealthy({ tasks }) {
+  const completedHandler = async (taskId) => {
+    const response = await fetch('http//localhost:3000/api/update-status', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ taskId: 1, date: '2023-03-29' })
+    })
+
+    if (response.ok) {
+      console.log(`task ${taskId} has been ticked`)
+    } else {
+      console.error(response.statusText)
+    }
+    console.log(taskId, ' from pages')
+  }
+
+  console.log(tasks)
+  return (
+		<div className="bg">
+			<HeaderCard
+				preHeaderText="Your challenges for:"
+				header="Get organised"
+				percentage={75}
+				textColor="black"
+				pathColor="var(--main-lavendar)"
+				trailColor="transparent"
+				width={110}
+				strokeWidth={10}
 			/>
-		</>
-	)
+			<div className="main-container">
+				<ChallengeCategoryList
+					tasks={tasks}
+					onCompleted={completedHandler}
+				/>
+			</div>
+						{/* <ul>
+				{tasks.map((task) => (
+					<li
+						key={task.id}
+						id={task.id}>
+						<h2>{task.name}</h2>
+						<p>{task.category_name}</p>
+					</li>
+				))}
+			</ul> */}
+		</div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const userId = 1
+  const response = await fetch(
+    `http://localhost:3000/api/get-healthy?userId=${userId}`
+  )
+  const tasks = await response.json()
+  console.log(tasks, ' getHealthy.js')
+
+  return {
+    props: {
+      tasks
+    }
+  }
 }
