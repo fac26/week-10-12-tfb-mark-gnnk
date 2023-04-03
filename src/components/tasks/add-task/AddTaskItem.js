1ceb96eb7bcfc880c4bbd7a037867312e2411fa9
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-04-03 15:18:20 +0100

    Add task item wrapped in a rectangle component used in the list

--- src/components/tasks/add-task/AddTaskItem.js
+++ src/components/tasks/add-task/AddTaskItem.js
@@ -1,5 +1,21 @@
-//task with button <Add> that listens for event when user clickes add button
-function AddTaskItem({ task }) {
-	return <></>
+import Button from 'components/UI/Button'
+import Rectangle from 'components/cards/Rectangle'
+import styles from './AddTaskItem.module.css'
+
+export default function AddTaskItem({ task }) {
+	return (
+		<Rectangle>
+			<li>
+				<div className={styles.title}>
+					<h3>{task.title}</h3>
+					<p>{task.description}</p>
+				</div>
+				<Button
+					scaling="small"
+					weight="bold">
+					ADD
+				</Button>
+			</li>
+		</Rectangle>
+	)
 }
-export default AddTaskItem
