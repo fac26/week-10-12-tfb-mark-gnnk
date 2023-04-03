fa3b2b3d9d7a5fbd2739113d225d2e68b3f0c126
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-04-03 15:19:33 +0100

    Initial setup of the add-task page

--- src/pages/add-task/index.js
+++ src/pages/add-task/index.js
@@ -1,14 +1,20 @@
-import Image from 'next/image'
-export default function AddTask() {
+import AddTaskList from 'components/tasks/add-task/AddTaskList'
+import Adviser from 'components/adviser/Adviser'
+import styles from 'styles/Home.module.css'
+
+export default function AddTaskPage() {
 	return (
-		<>
-			<h1>Add micro habits</h1>
-			<Image
-				src="/figma/add-task.png"
-				width={300}
-				height={600}
-				alt="days screenshot"
-			/>
-		</>
+		<div className="bg">
+			<div className="main-container">
+				<div className={`${styles.adviser} main-container`}>
+					<Adviser
+						src="/icons/bear.png"
+						text="If you’ve already completed all the challenges. You can add some more micro-habits to earn more coins."
+						speechBg={`var(--main-white)`}
+					/>
+				</div>
+				<AddTaskList />
+			</div>
+		</div>
 	)
 }
