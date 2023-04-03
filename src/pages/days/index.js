b0df1f3fc2af813aecc56764a04c1dcbcdb392a7
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-04-03 12:29:21 +0100

    rendered days page at days/index

--- src/pages/days/index.js
+++ src/pages/days/index.js
@@ -1,14 +1,39 @@
-import Image from 'next/image'
+import Adviser from 'components/adviser/Adviser'
+import styles from '../../styles/Days.module.css'
+import DayList from '../../components/days/DayList'
+
 export default function Days() {
 	return (
-		<>
-			<h1>Calendar</h1>
-			<Image
-				src="/figma/days.png"
-				width={300}
-				height={600}
-				alt="days screenshot"
-			/>
-		</>
+		<div className="bg">
+			<div className={`${styles.adviser} main-container`}>
+				<Adviser
+					src="/icons/bear.png"
+					text="Welcome back! Click on one of the days to see how you're getting on with your habits."
+					speechBg={`var(--main-white)`}
+				/>
+			</div>
+			<div className={`${styles.flex} ${styles.center} main-container`}>
+				<DayList />
+			</div>
+		</div>
 	)
 }
+
+// export async function getServerSideProps(context) {
+// 	//const userId = context.req.session.userId
+
+// 	const userId = 1
+// 	const today = new Date().toISOString().split('T')[0]
+// 	const response = await fetch(
+// 		`http://localhost:3000/api/tasks?userId=${userId}&date=${today}`
+// 	)
+
+// 	const score = await response.json()
+// 	console.log(score, ' home page getServerSideProps')
+
+// 	return {
+// 		props: {
+// 			score: score.score
+// 		}
+// 	}
+// }
