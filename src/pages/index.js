bff8b8043f906d323475334958f136e5922685de
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-04-03 14:43:27 +0100

    add landing page styles and components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import Circle from 'components/cards/ProgressCircle'
-import Image from 'next/image'
+import Logo from 'components/UI/Logo'
 import Link from 'next/link'
+import Button from 'components/UI/Button'
+import styles from '../styles/Index.module.css'
 export default function Home() {
 	return (
-		<>
-			<Link href="/home">Ready? Let&rsquo;s go</Link>
-			<Image
-				src="/figma/landing.png"
-				width={300}
-				height={600}
-				alt="days screenshot"
-			/>
-		</>
+		<div className="landing-bg">
+			<div className={styles['landing-container']}>
+				<Logo />
+				<div className={styles.about}>
+					An app to help you build good habits and get rid of the bad ones.
+				</div>
+				<Button
+					background="bg_wht"
+					scaling="large">
+					<Link href="/home">Ready? Let&rsquo;s go</Link>
+				</Button>
+			</div>
+		</div>
 	)
 }
