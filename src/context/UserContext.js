68219dd6c226f772d80d6e75402942967a282150
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-04-04 00:07:29 +0100

    add avatar and functon to update avatar

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,7 +1,12 @@
 //import { getUserPoints } from '../model/user-profile.js'
 import { createContext, useContext, useState, useEffect } from 'react'
 
-const UserContext = createContext({ points: '', updateUserPoints: () => {} })
+const UserContext = createContext({
+	points: '',
+	updateUserPoints: () => {},
+	avatar: '',
+	updateUserAvatar: () => {}
+})
 
 export const useUserContext = () => {
 	return useContext(UserContext)
@@ -9,7 +14,7 @@ export const useUserContext = () => {
 
 export const UserProvider = ({ children, userId }) => {
 	const [points, setPoints] = useState(0)
-
+	const [avatar, setAvatar] = useState(1)
 	useEffect(() => {
 		const fetchInitialPoints = async () => {
 			try {
@@ -20,16 +25,27 @@ export const UserProvider = ({ children, userId }) => {
 				console.error('Error fetching points:', error)
 			}
 		}
+		const fetchInitialAvatar = async () => {
+			const response = await fetch(`/api/get-userAvatar?userId=${userId}`)
+			const { userAvatar } = await response.json()
+			console.log(userAvatar, ' fetch avatar')
+			setAvatar(userAvatar)
+		}
 
 		fetchInitialPoints()
+		fetchInitialAvatar()
 	}, [userId])
 
 	const updateUserPoints = (newPoints) => {
 		setPoints((currentPoints) => currentPoints + newPoints)
 	}
+	const updateUserAvatar = (newAvatar) => {
+		setAvatar(newAvatar)
+	}
 
 	return (
-		<UserContext.Provider value={{ points, updateUserPoints }}>
+		<UserContext.Provider
+			value={{ points, updateUserPoints, avatar, updateUserAvatar }}>
 			{children}
 		</UserContext.Provider>
 	)
