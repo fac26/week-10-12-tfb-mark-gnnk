f087e6bd24ac375dfd6ae7be91300bfa2835012b
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-04-04 16:50:12 +0100

    update confirmation button, import custom Button
    Co-authored-by: Konstantina  <konstantinakatmada@users.noreply.github.com>

--- src/components/modals/ConfirmationModal.js
+++ src/components/modals/ConfirmationModal.js
@@ -1,14 +1,26 @@
+import Button from 'components/UI/Button'
 import Modal from '../UI/Modal'
+import styles from './ConfirmationModal.module.css'
 export default function ConfirmationModal({ show, onConfirm, onCancel }) {
 	if (!show) {
 		return null
 	}
 
 	return (
-		<Modal>
+		<Modal className={styles.container}>
 			<p>Are you sure you’ve completed this micro-habit?</p>
-			<button onClick={onConfirm}>Yes</button>
-			<button onClick={onCancel}>No</button>
+			<div className={styles.btns}>
+				<Button
+					onClick={onConfirm}
+					background="bg_purple">
+					Yes
+				</Button>
+				<Button
+					onClick={onCancel}
+					background="bg_purple">
+					No
+				</Button>
+			</div>
 		</Modal>
 	)
 }
