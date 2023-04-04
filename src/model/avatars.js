77c3d28551f073c1f9a842a951cfed6757843e11
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-04-04 14:29:09 +0100

    add user avatar query
    Co-authored-by: Karol <105356599+Kr33L@users.noreply.github.com>
    Co-authored-by: Niete <101563800+Psydwinder@users.noreply.github.com>
    Co-authored-by: Konstantina <a110138008+konstantinakatmada@users.noreply.github.com
    Co-authored-by: Georgia <81175151+rockyrelay@users.noreply.github.com>

--- src/model/avatars.js
+++ src/model/avatars.js
@@ -1,4 +1,4 @@
-import db from 'database/db.js'
+import db from '../database/db.js'
 
 const select_all_avatars = db.prepare(
 	/*sql*/
@@ -11,3 +11,16 @@ const select_all_avatars = db.prepare(
 export function getAllAvatars() {
 	return select_all_avatars.all()
 }
+
+const insert_user_avatar = db.prepare(
+	/*sql*/
+	`INSERT INTO user_avatars (user_id, avatar_id)
+VALUES (?, ?)`
+)
+
+export function addUserAvatar(userId, avatarId) {
+	return insert_user_avatar.run(userId, avatarId)
+}
+
+// const result = addUserAvatar(1, 5)
+// console.log(result)
