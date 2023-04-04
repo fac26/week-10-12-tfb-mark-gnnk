683dd748f2f0c97166a141e3bb29bd6c06a7914d
Natalia Rusu <nataliarusu.rn@gmail.com>
2023-04-04 16:51:15 +0100

    toggle backdrop

--- src/components/challenges/ChallengeCategoryList.js
+++ src/components/challenges/ChallengeCategoryList.js
@@ -1,8 +1,11 @@
 import ChallengeCategoryItem from './ChallengeCategoryItem'
 import { useState } from 'react'
 import ConfirmationModal from '../modals/ConfirmationModal'
+import Backdrop from 'components/UI/Backdrop'
 export default function ChallengeCategoryList({ tasks, onCompleted }) {
 	const [showConfirmationModal, setShowConfirmationModal] = useState(false)
+	const [showBackdrop, setShowBackdrop] = useState(false)
+
 	const [taskIdToComplete, setTaskIdToComplete] = useState(null)
 
 	const taskCheckedHandler = (event) => {
@@ -10,14 +13,17 @@ export default function ChallengeCategoryList({ tasks, onCompleted }) {
 
 		setTaskIdToComplete(event.target.id)
 		setShowConfirmationModal(true)
+		setShowBackdrop(true)
 	}
 	const confirmCompleteTask = () => {
 		setShowConfirmationModal(false)
+		setShowBackdrop(false)
 		onCompleted(taskIdToComplete)
 		setTaskIdToComplete(null)
 	}
 	const cancelComplete = () => {
 		setShowConfirmationModal(false)
+		setShowBackdrop(false)
 	}
 	return (
 		<ul>
@@ -29,7 +35,7 @@ export default function ChallengeCategoryList({ tasks, onCompleted }) {
 					complete={task.status} //task.status
 				/>
 			))}
-
+			{showBackdrop && <Backdrop opacity={0.5} />}
 			<ConfirmationModal
 				show={showConfirmationModal}
 				onConfirm={confirmCompleteTask}
