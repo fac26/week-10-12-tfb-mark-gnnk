5d5d69be3398ea9cbfed0cbf26edb50667b63c73
rockyrelay <rockyrelay@users.noreply.github.com>
2023-04-05 11:25:17 +0000

    Prettified Code!

--- cypress/e2e/challenge-progress.nav.cy.js
+++ cypress/e2e/challenge-progress.nav.cy.js
@@ -1,24 +1,26 @@
 describe('The progress bars correctly reflect the percentage of tasks completed', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000/challenges/get-healthy')
-  })
+	beforeEach(() => {
+		cy.visit('http://localhost:3000/challenges/get-healthy')
+	})
 
 	it('has accessed the get-healthy challenge page', () => {
-    cy.url().should('include', '/get-healthy')
-  })
+		cy.url().should('include', '/get-healthy')
+	})
 
-  it('can find the task to select', () => {
-    cy.get('#\\37 -category-3 > div > div')
-  })
+	it('can find the task to select', () => {
+		cy.get('#\\37 -category-3 > div > div')
+	})
 
-  it('is able to toggle the completion selector and render the task completed', () => {
-    cy.get('#\\37').click()
-    cy.get('#__next > div.Layout_main__rFqb2 > div > div.main-container > ul > div > button:nth-child(2)').click()
-  })
+	it('is able to toggle the completion selector and render the task completed', () => {
+		cy.get('#\\37').click()
+		cy.get(
+			'#__next > div.Layout_main__rFqb2 > div > div.main-container > ul > div > button:nth-child(2)'
+		).click()
+	})
 
-  it('updates the progress bar to show a task has been completed', () => {
-    cy.get('#__next > div.Layout_main__rFqb2 > div > div.top-container.HeaderCard_container__STGvs > div:nth-child(2) > div > svg > text').contains('33')
-  })
+	it('updates the progress bar to show a task has been completed', () => {
+		cy.get(
+			'#__next > div.Layout_main__rFqb2 > div > div.top-container.HeaderCard_container__STGvs > div:nth-child(2) > div > svg > text'
+		).contains('33')
+	})
 })
-
-
